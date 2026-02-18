830c079251bda59018daa55d5f3dedafde63dcf0
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2026-02-18 23:40:43 +0200

    Fixed stilistic errors 2.0

--- app/shop.py
+++ app/shop.py
@@ -27,9 +27,9 @@ class Shop:
         )
 
     def print_receipt(self, customer: Customer) -> None:
-        print(f"Date: {datetime.datetime.now().strftime(
+        print(f'Date: {datetime.datetime.now().strftime(
             "%d/%m/%Y %H:%M:%S"
-        )}")
+        )}')
         print(f"Thanks, {customer.name}, for your purchase!")
         print("You have bought:")
 
